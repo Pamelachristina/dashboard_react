48441ed059a5b981afd1b8c9df795bbc09cd1841
User Office <pamelachristina@mac.com>
2024-06-07 16:42:12 -0700

    updated readme

---
+++ user-dashboard/src/pages/Overview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import KeyMetrics from '../components/KeyMetrics';
+
+const Overview = () => (
+  <div>
+    <KeyMetrics />
+  </div>
+);
+
+export default Overview;
---
+++ user-dashboard/src/pages/UserStats.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserStats = () => (
+  <div>
+    <h2>User Stats</h2>
+    {/* Add your user stats component or code here */}
+  </div>
+);
+
+export default UserStats;
