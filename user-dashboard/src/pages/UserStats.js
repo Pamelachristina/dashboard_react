a1ae5cb590e4fea6daeeed6e3758e93acfdc9922
User Office <pamelachristina@mac.com>
2024-07-15 16:29:54 -0700

    made adjustments to overview.js and started userstats pg. Remember to make a new branch for the userstats pg

--- user-dashboard/src/pages/UserStats.js
+++ user-dashboard/src/pages/UserStats.js
@@ -1,10 +1,65 @@
+// src/pages/UserStats.js
 import React from 'react';
+import StatsCard from "../components/StatsCard";
+import User from '../components/User';
 
 const UserStats = () => (
   <div>
-    <h2>User Stats</h2>
-    {/* Add your user stats component or code here */}
+    <User />
+    <div className="main-content">
+      <div className="container-fluid">
+        <div className="row mb-4">
+          <div className="col-md-4">
+            <StatsCard title="more details about institutions/companies based on fiscal year"  />
+          </div>
+          <div className="col-md-4">
+            <StatsCard title="Name of Uni that is MSI / Epsore" />
+          </div>
+          <div className="col-md-4">
+            <StatsCard title="Number of users from each school" />
+          </div>
+          <div className="col-md-4">
+            <StatsCard title="First year ? or Returning" />
+          </div>
+        </div>
+        <div className="row mb-4">
+          <div className="col-md-8">
+            <div className="content-box d-flex">
+              <div className="w-50">
+                
+               
+                 
+              
+              <div className="w-50 d-flex">
+               
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 d-flex flex-column align-items-center justify-content-center content-box">
+            
+           
+            
+            <div className="globe-container">
+             
+            </div>
+          </div>
+        </div>
+        <div className="row mb-4">
+          <div className="col-md-4">
+            <StatsCard title="Placeholder" />
+          </div>
+          <div className="col-md-4">
+            <StatsCard title="Placeholder"  />
+          </div>
+          <div className="col-md-4">
+            <StatsCard title="Placeholder"  />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 );
 
 export default UserStats;
+
